874f0cbeab38b781c7c6ef9301f02f69eb603fd3
chrisatyler <ctyler@us.ibm.com>
2018-08-15 19:56:17 -0500

    updated the wolfpack-fitclub-fitcoin bna

--- fabric-dev-servers/downloadFabric.sh
+++ fabric-dev-servers/downloadFabric.sh
@@ -11,4 +11,5 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+FABRIC_VERSION=hlfv12
 source "$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"/_loader.sh
